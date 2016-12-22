35d49ee915c57ab7397ee3e83fd0725070fc760f
Tim Blair <trblair@gmail.com>
2016-12-22 16:40:27 -0500

    Single and group calls now  return the same type. Added some more tests. Try a bit harder not to draw from within the same group

--- __tests__/randomizer.spec.js
+++ __tests__/randomizer.spec.js
@@ -9,10 +9,44 @@ test('Everyone should be a giver and a receiver', () => {
     const result = randomizer.group(groups);
 
     const keys = new Set(R.flatten(groups)),
-      resultKeys = new Set(result.keys()),
-      keyDifference = new Set([...resultKeys].filter(x => !keys.has(x))),
-      resultValues = new Set(result.keys()),
-      valueDifference = new Set([...resultValues].filter(x => !keys.has(x)));
-    expect(keyDifference.size).toBe(0);
-    expect(valueDifference.size).toBe(0);
+      resultKeys = new Set(),
+      resultValues = new Set();
+    for(let [key, value] of result) {
+      resultKeys.add(key);
+      resultValues.add(value);
+    }
+    expect(result.size).toBe(keys.size);
+    expect(resultKeys.size).toBe(keys.size);
+    expect(resultValues.size).toBe(keys.size);
+});
+
+test('Single: Everyone should be a giver and a receiver', () => {
+    const randomizer = require('../randomizer');
+    const names = ['a', 'b', 'c', '1', '2', '3'];
+    const result = randomizer.single(names);
+
+    const keys = new Set(names),
+      resultKeys = new Set(),
+      resultValues = new Set();
+    for(let [key, value] of result) {
+      resultKeys.add(key);
+      resultValues.add(value);
+    }
+    expect(result.size).toBe(keys.size);
+    expect(resultKeys.size).toBe(keys.size);
+    expect(resultValues.size).toBe(keys.size);
+});
+
+test('No one should ever draw themselves', () => {
+    const randomizer = require('../randomizer');
+    const R = require('ramda');
+    const groups = [
+      ['a', 'b'],
+      ['1', '2']
+    ];
+    const result = randomizer.group(groups);
+
+    for(let [key, value] of result) {
+      expect(key).not.toBe(result.get(key))
+    }
 });
--- randomizer.js
+++ randomizer.js
@@ -1,31 +1,57 @@
 // @flow
 
 const shuffle = require('shuffle-array');
+const R = require('ramda');
 
-const single = (names: string[]): string[] => {
-  return shuffle(names.map((name) => name.trim()));
+const trimAndShuffle = (names: string[]): string[] => {
+  return shuffle(names.map(n => n.trim()));
 };
 
-const group = (groups: string[][]): Map<string, string> => {
-  const names = groups.map((pair) => {
-    return shuffle(pair.map((s) => s.trim()));
+const single = (names: string[]): Map<string, string> => {
+  const shuffled = trimAndShuffle(names);
+  const result = new Map();
+  shuffled.forEach((name, index) => {
+    const next = new WrappingNumber(shuffled.length, index).next();
+    result.set(name, shuffled[next]);
   });
+  return result;
+};
 
-  const people = shuffle(names),
-    result = new Map(),
+// Tries to match people up so that no one draws from their own group, returns null if it fails
+const mapGroups = (groups: string[][]): Map<string, string> | null => {
+  const result = new Map(),
     used = new Set();
-  people.forEach((group, index) => {
-    const next = new WrappingNumber(people.length, index);
-    group.forEach(member => {
-      let unused = [];
-      // todo: will select from same group in some conditions
-      while(unused.length === 0) {
-        unused = people[next.next()].filter(e => !used.has(e));
+
+  for(const [index, group] of groups.entries()) {
+    const next = new WrappingNumber(groups.length, index);
+    for(const member of group) {
+      let unused = [],
+        loops = 0;
+      while(unused.length === 0 && loops < 2) {
+        const target = next.next();
+        if(target === index) {
+          loops++;
+          continue;
+        }
+        unused = groups[target].filter(e => !used.has(e));
+      }
+      if(unused.length === 0) {
+        return null;
       }
       result.set(member, unused[0]);
       used.add(unused[0]);
-    });
-  });
+    }
+  }
+  return result;
+};
+
+const group = (groups: string[][]): Map<string, string> => {
+  const people = shuffle(groups.map(trimAndShuffle)),
+    result = mapGroups(people);
+
+  if(result === null) {
+    return single(R.flatten(groups));
+  }
 
   return result;
 }
@@ -39,8 +65,8 @@ class WrappingNumber {
   }
 
   next() {
-    this.current = this.current + 1;
-    if(this.current == this.max) {
+    this.current++;
+    if(this.current === this.max) {
       this.current = 0;
     }
     return this.current;
